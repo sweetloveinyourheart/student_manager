c362a961c9b21e5d1bb2ea29f24f6dd68334e208
Thanh-Dat <vuongthanhdat2703@gmail.com>
2022-12-14 11:14:51 +0700

    DTO updated

--- DAO/KetQuaDAO.cs
+++ DAO/KetQuaDAO.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO;
 
 namespace DAO
 {
-    internal class KetQuaDAO
+    public class KetQuaDAO
     {
+        private static KetQuaDAO instance;
+
+        public static KetQuaDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new KetQuaDAO();
+                return instance;
+            }
+        }
+
+        private KetQuaDAO() { }
+
+        QuanLyDiemDataContext db = new QuanLyDiemDataContext();
+        public List<KetQua> FormLoad()
+        {
+            List<KetQua> list = new List<KetQua>();
+
+            list = db.tblKET_QUAs.Select(s => new KetQua(
+                   s.MaSV,
+                   s.HoTen,
+                   s.MaLop,
+                   s.MaMon,
+                   s.DiemTB,
+                   s.DiemThiLan1,
+                   s.DiemThiLan2,
+                   s.DiemTongKet,
+                   s.HanhKiem,
+                   s.HocKi,
+                   s.GhiChu
+                )
+            ).ToList();
+
+            return list;
+        }
     }
 }
